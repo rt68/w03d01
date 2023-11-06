d13d90db03eaf837e779c06700898797a9b226fa
rt68 <54224071+rt68@users.noreply.github.com>
2023-11-06 12:46:22 -0500

    created timmys story

--- homework.js
+++ homework.js
@@ -14,6 +14,12 @@ class Hamster {
       return this.price;
     }
   }
+   //  const hamster1 = new Hamster("Mao");
+   //  hamster1.wheelRun();
+   //  hamster1.eatFood();
+   // console.log(hamster1.getPrice());
+   //  console.log(hamster1)
+  
   class Person {
     constructor(name) {
       this.name = name;
@@ -55,3 +61,32 @@ class Hamster {
       this.mood += 10;
       this.bankAccount -= hamster.getPrice();
     }
+  }
+  //Instantiate a new Person named Timmy
+   const timmy = new Person('Timmy');
+  //Age Timmy five years
+   for (let i = 0; i < 5; i++) {
+   timmy.ageUp();
+   }
+  //At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+   for (let i = 0; i < 5; i++) { timmy.eat();
+  }
+  //Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+  for (let i = 0; i < 5; i++) { timmy.exercise();
+                              }
+  //Age Timmy 9 years
+  for (let i = 0; i < 9; i++) {
+  timmy.ageUp();
+  }
+  
+  const gus = new Hamster('Gus');
+  gus.owner = 'Timmy';
+  timmy.buyHamster(gus);
+  //Age Timmy 15 years
+  for (let i = 0; i < 15; i++) {  timmy.ageUp(15); }       
+  //Have Timmy eat twice                
+  for (let i = 0; i < 2; i++) { timmy.eat();}
+  //Have Timmy exercise twice
+  for (let i = 0; i < 2; i++) { timmy.exercise();}
+  
+  console.log(timmy);
